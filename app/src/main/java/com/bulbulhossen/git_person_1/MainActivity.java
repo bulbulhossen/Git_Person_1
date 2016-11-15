ceff2becca0c665d6ac57e427a3f0c93b56aacb6
Md.Bulbul Hossen <info247.bulbul@gmail.com>
2016-11-16 02:22:11 +0600

    Initial commit

---
+++ app/src/main/java/com/bulbulhossen/git_person_1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bulbulhossen.git_person_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //First Change
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
